d322716143c9ba26c10d2112cd90ed143d111a88
cheval_fromage <ant.thiery@free.fr>
2025-07-01 15:58:21 +0900

    le robot fait des pompes

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,23 @@
 
 #define DXL_SERIAL     Serial1
 #define DXL_DIR_PIN    2
-#define DXL1_ID         3
-#define DXL2_ID         1
+#define DXL0_ID         10
+#define DXL1_ID         11
+#define DXL2_ID         12
 #define DXL_BAUDRATE   1000000
 
 Dynamixel2Arduino dxl(DXL_SERIAL, DXL_DIR_PIN);
+
+void deplacement(int debut0, int fin0,int debut1, int fin1){
+  float etapes = 200.0;
+  float pas0= ((float)(fin0-debut0))/etapes;
+  float pas1= ((float)(fin1-debut1))/etapes;
+  for(int i=0; i<etapes; i++){
+    dxl.setGoalPosition(DXL0_ID, debut0 + pas0*i);
+    dxl.setGoalPosition(DXL1_ID, debut1 + pas1*i);
+    delay(10);
+  }
+}
 
 void setup() {
   Serial.begin(115200);
@@ -15,6 +27,13 @@ void setup() {
   DXL_SERIAL.begin(DXL_BAUDRATE);
   dxl.begin();
   dxl.setPortProtocolVersion(2.0);
+
+  if (!dxl.ping(DXL0_ID)) {
+    Serial.println("Servo 0 non détecté !");
+    while (1);
+  } else {
+    Serial.println("Servo 0 détecté !");
+  }
 
   if (!dxl.ping(DXL1_ID)) {
     Serial.println("Servo 1 non détecté !");
@@ -30,6 +49,10 @@ void setup() {
     Serial.println("Servo 2 détecté !");
   }
 
+  dxl.torqueOff(DXL0_ID);
+  dxl.setOperatingMode(DXL0_ID, OP_POSITION);
+  dxl.torqueOn(DXL0_ID);
+
   dxl.torqueOff(DXL1_ID);
   dxl.setOperatingMode(DXL1_ID, OP_POSITION);
   dxl.torqueOn(DXL1_ID);
@@ -42,10 +65,6 @@ void setup() {
 }
 
 void loop() {
-  dxl.setGoalPosition(DXL1_ID, 1025);
-  dxl.setGoalPosition(DXL2_ID, 1025);
-  delay(3000);
-  dxl.setGoalPosition(DXL1_ID, 2000);
-  dxl.setGoalPosition(DXL2_ID, 2000);
-  delay(3000);
+  deplacement(2048, 2600, 1535, 3200);
+  deplacement(2600, 2048, 3200, 1535);
 }
